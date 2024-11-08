a2f3540808d5eae29967edca4a42cee041190164
Neil <me@neilrichter.com>
2024-11-08 19:15:00 +0100

    feat: add devtools client

---
+++ client/nuxt.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'pathe'
+
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/devtools-ui-kit',
+  ],
+  ssr: false,
+
+  devtools: {
+    enabled: false,
+  },
+
+  app: {
+    baseURL: '/__nuxt-drizzle',
+  },
+
+  compatibilityDate: '2024-08-21',
+
+  nitro: {
+    output: {
+      publicDir: resolve(__dirname, '../dist/client'),
+    },
+  },
+
+  vite: {
+    server: {
+      hmr: {
+        // Instead of go through proxy, we directly connect real port of the client app
+        clientPort: +(process.env.PORT || 3400),
+      },
+    },
+  },
+})
